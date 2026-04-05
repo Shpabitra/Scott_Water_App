25a68907bd617a94c7f3cdb0d6c7bb382a56aeac
Pabitra Shrestha <30482092@cityofglacol.ac.uk>
2026-04-05 19:10:35 +0100

    Changes made in register new business

--- Scott_Water_App/Register_New_Business.Designer.cs
+++ Scott_Water_App/Register_New_Business.Designer.cs
@@ -43,8 +43,8 @@
             this.txtTelephone = new System.Windows.Forms.TextBox();
             this.TxtEmail = new System.Windows.Forms.TextBox();
             this.txtContactPerson = new System.Windows.Forms.TextBox();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,9 +53,10 @@
             this.btnMenu.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnMenu.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnMenu.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnMenu.Location = new System.Drawing.Point(30, 26);
+            this.btnMenu.Location = new System.Drawing.Point(45, 40);
+            this.btnMenu.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnMenu.Name = "btnMenu";
-            this.btnMenu.Size = new System.Drawing.Size(198, 65);
+            this.btnMenu.Size = new System.Drawing.Size(175, 90);
             this.btnMenu.TabIndex = 0;
             this.btnMenu.Text = "Scot Water";
             this.btnMenu.UseVisualStyleBackColor = false;
@@ -65,9 +66,10 @@
             this.btnExit.BackColor = System.Drawing.Color.LightSteelBlue;
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnExit.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnExit.Location = new System.Drawing.Point(30, 661);
+            this.btnExit.Location = new System.Drawing.Point(45, 635);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(175, 65);
+            this.btnExit.Size = new System.Drawing.Size(175, 90);
             this.btnExit.TabIndex = 1;
             this.btnExit.Text = "EXIT";
             this.btnExit.UseVisualStyleBackColor = false;
@@ -76,7 +78,8 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(124, 149);
+            this.label1.Location = new System.Drawing.Point(186, 229);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(274, 37);
             this.label1.TabIndex = 2;
@@ -86,7 +89,8 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(242, 208);
+            this.label2.Location = new System.Drawing.Point(298, 287);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(162, 37);
             this.label2.TabIndex = 3;
@@ -96,7 +100,8 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(77, 325);
+            this.label3.Location = new System.Drawing.Point(133, 395);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(327, 37);
             this.label3.TabIndex = 4;
@@ -106,7 +111,8 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(209, 264);
+            this.label4.Location = new System.Drawing.Point(265, 341);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(195, 37);
             this.label4.TabIndex = 5;
@@ -116,7 +122,8 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(139, 392);
+            this.label5.Location = new System.Drawing.Point(185, 449);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(259, 37);
             this.label5.TabIndex = 6;
@@ -126,7 +133,8 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(136, 450);
+            this.label6.Location = new System.Drawing.Point(182, 503);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(262, 37);
             this.label6.TabIndex = 7;
@@ -137,9 +145,10 @@
             this.btnRegister.BackColor = System.Drawing.Color.DodgerBlue;
             this.btnRegister.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnRegister.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.btnRegister.Location = new System.Drawing.Point(465, 547);
+            this.btnRegister.Location = new System.Drawing.Point(601, 592);
+            this.btnRegister.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnRegister.Name = "btnRegister";
-            this.btnRegister.Size = new System.Drawing.Size(175, 65);
+            this.btnRegister.Size = new System.Drawing.Size(175, 90);
             this.btnRegister.TabIndex = 8;
             this.btnRegister.Text = "Register";
             this.btnRegister.UseVisualStyleBackColor = false;
@@ -147,77 +156,85 @@
             // txtBusinessName
             // 
             this.txtBusinessName.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtBusinessName.Location = new System.Drawing.Point(404, 142);
+            this.txtBusinessName.Location = new System.Drawing.Point(452, 233);
+            this.txtBusinessName.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtBusinessName.Name = "txtBusinessName";
-            this.txtBusinessName.Size = new System.Drawing.Size(319, 44);
+            this.txtBusinessName.Size = new System.Drawing.Size(405, 44);
             this.txtBusinessName.TabIndex = 9;
             // 
             // txtAddress
             // 
             this.txtAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAddress.Location = new System.Drawing.Point(404, 205);
+            this.txtAddress.Location = new System.Drawing.Point(452, 287);
+            this.txtAddress.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtAddress.Name = "txtAddress";
-            this.txtAddress.Size = new System.Drawing.Size(319, 44);
+            this.txtAddress.Size = new System.Drawing.Size(405, 44);
             this.txtAddress.TabIndex = 10;
             // 
             // txtPostCode
             // 
             this.txtPostCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPostCode.Location = new System.Drawing.Point(404, 264);
+            this.txtPostCode.Location = new System.Drawing.Point(452, 341);
+            this.txtPostCode.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtPostCode.Name = "txtPostCode";
-            this.txtPostCode.Size = new System.Drawing.Size(319, 44);
+            this.txtPostCode.Size = new System.Drawing.Size(405, 44);
             this.txtPostCode.TabIndex = 11;
             // 
             // txtTelephone
             // 
             this.txtTelephone.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTelephone.Location = new System.Drawing.Point(404, 322);
+            this.txtTelephone.Location = new System.Drawing.Point(452, 395);
+            this.txtTelephone.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtTelephone.Name = "txtTelephone";
-            this.txtTelephone.Size = new System.Drawing.Size(319, 44);
+            this.txtTelephone.Size = new System.Drawing.Size(405, 44);
             this.txtTelephone.TabIndex = 12;
             // 
             // TxtEmail
             // 
             this.TxtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TxtEmail.Location = new System.Drawing.Point(404, 385);
+            this.TxtEmail.Location = new System.Drawing.Point(452, 449);
+            this.TxtEmail.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TxtEmail.Name = "TxtEmail";
-            this.TxtEmail.Size = new System.Drawing.Size(319, 44);
+            this.TxtEmail.Size = new System.Drawing.Size(405, 44);
             this.TxtEmail.TabIndex = 13;
             // 
             // txtContactPerson
             // 
             this.txtContactPerson.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtContactPerson.Location = new System.Drawing.Point(404, 450);
+            this.txtContactPerson.Location = new System.Drawing.Point(452, 503);
+            this.txtContactPerson.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.txtContactPerson.Name = "txtContactPerson";
-            this.txtContactPerson.Size = new System.Drawing.Size(319, 44);
+            this.txtContactPerson.Size = new System.Drawing.Size(405, 44);
             this.txtContactPerson.TabIndex = 14;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::Scott_Water_App.Properties.Resources.Scot_Water_App_Logo;
-            this.pictureBox1.Location = new System.Drawing.Point(762, 26);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 93);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 15;
-            this.pictureBox1.TabStop = false;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(332, 732);
+            this.label7.Location = new System.Drawing.Point(498, 1126);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(317, 20);
             this.label7.TabIndex = 16;
             this.label7.Text = "NOTE:  Please fill all the required field.";
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Scott_Water_App.Properties.Resources.Scot_Water_App_Logo;
+            this.pictureBox1.Location = new System.Drawing.Point(800, 20);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(145, 135);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
             // Register_New_Business
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
             this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
-            this.ClientSize = new System.Drawing.Size(884, 761);
+            this.ClientSize = new System.Drawing.Size(984, 761);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.txtContactPerson);
@@ -235,6 +252,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnMenu);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Register_New_Business";
             this.Text = "Register_New_Business";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
